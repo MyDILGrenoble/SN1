bd52a91e1d533bf5a459adf59c79302266f465c4
MyDIL Grenoble <161693680+MyDILGrenoble@users.noreply.github.com>
2024-03-05 16:14:57 +0100

    Update functions.php

--- ProjetWeb/functions.php
+++ ProjetWeb/functions.php
@@ -7,8 +7,8 @@ function pdo_connect_mysql() {
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
-    	// If there is an error with the connection, stop the script and display the error.
-    	exit('Failed to connect to database!');
+    	// Si la connection échoue, on arrête tout et on affiche l'erreur.
+    	exit('La connexion a échoué!');
     }
 }
 function template_header($title) {
